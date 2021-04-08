f500513b3d600480f28021eebd3395cb4f20a890
Stuart Finley <finleysg@gmail.com>
2021-04-08 05:54:14 -0500

    match play reg is over for the year

--- src/pages/MatchPlayPage.tsx
+++ src/pages/MatchPlayPage.tsx
@@ -11,7 +11,19 @@ const MatchPlayPage: React.FC = () => {
             RightColumn={
                 <React.Fragment>
                     <h3 className="text-primary">{constants.MatchPlayYear} Match Play Teams</h3>
-                    <p>Sign up procedures for 2021 are TBD</p>
+                    <p>2021 team listing coming soon...</p>
+                    {/* <div>
+                        <iframe
+                            title="2021-sign-up"
+                            src="https://docs.google.com/forms/d/e/1FAIpQLSfISGwD9LJepombufKhw4l0wBZj2EkfP2RnlbbSRn_YeeIHeQ/viewform?embedded=true"
+                            width="640"
+                            height="1522"
+                            frameBorder="0"
+                            marginHeight={0}
+                            marginWidth={0}>
+                            Loading…
+                        </iframe>
+                    </div> */}
                     {/* <TeamList /> */}
                 </React.Fragment>
             }
