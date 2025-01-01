ca2074faf3dfe3c0e2b7b117d777ba4354a4337c
Stuart Finley <finleysg@gmail.com>
2025-01-01 13:50:53 -0600

    Fixes a pair of rendering problems with bs v5

--- src/features/events/EventDetailView.tsx
+++ src/features/events/EventDetailView.tsx
@@ -1,25 +1,23 @@
-import * as React from "react";
+import * as React from "react"
 
-import EventChairList from "./EventChairList";
-import EventLocationView from "./EventLocationView";
-import { EventProps } from "./eventsPropType";
-import EventFormatDetail from "./format/EventFormatDetail";
-import EventPointsList from "./points/EventPointsList";
-import EventPolicyList from "./policies/EventPolicyList";
+import EventLocationView from "./EventLocationView"
+import { EventProps } from "./eventsPropType"
+import EventFormatDetail from "./format/EventFormatDetail"
+import EventPointsList from "./points/EventPointsList"
+import EventPolicyList from "./policies/EventPolicyList"
 
 const EventDetailView: React.FC<EventProps> = (props) => {
-  const { eventDetail } = props;
+	const { eventDetail } = props
 
-  return (
-    <div>
-      <h1 className="text-secondary">{eventDetail.name}</h1>
-      <EventLocationView eventDetail={eventDetail} />
-      <EventFormatDetail eventDetail={eventDetail} />
-      <EventPolicyList eventDetail={eventDetail} />
-      <EventPointsList eventDetail={eventDetail} />
-      <EventChairList eventDetail={eventDetail} />
-    </div>
-  );
-};
+	return (
+		<div>
+			<h1 className="text-secondary">{eventDetail.name}</h1>
+			<EventLocationView eventDetail={eventDetail} />
+			<EventFormatDetail eventDetail={eventDetail} />
+			<EventPolicyList eventDetail={eventDetail} />
+			<EventPointsList eventDetail={eventDetail} />
+		</div>
+	)
+}
 
-export default EventDetailView;
+export default EventDetailView
--- src/features/tournaments/TournamentView.tsx
+++ src/features/tournaments/TournamentView.tsx
@@ -1,67 +1,70 @@
-import * as React from "react";
-import { useEffect, useRef } from "react";
+import * as React from "react"
+import { useEffect, useRef } from "react"
 
-import { NavLink, useLocation } from "react-router-dom";
-import styled from "styled-components";
+import { NavLink, useLocation } from "react-router-dom"
+import styled from "styled-components"
 
-import constants from "../../app-constants";
-import MarkdownRender from "../../components/MarkdownRender";
-import { TournamentDetailProps } from "./tournamentPropTypes";
+import constants from "../../app-constants"
+import MarkdownRender from "../../components/MarkdownRender"
+import { TournamentDetailProps } from "./tournamentPropTypes"
 
 const LogoImage = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  img {
-    width: 75px;
-    height: 75px;
-    background-size: contain;
-    object-fit: scale-down;
-  }
+	position: absolute;
+	top: 0;
+	right: 0;
+	img {
+		width: 75px;
+		height: 75px;
+		background-size: contain;
+		object-fit: scale-down;
+	}
 `
 LogoImage.displayName = "LogoImage"
 
 const TournamentView: React.FC<TournamentDetailProps> = (props) => {
-  const { tournament, logoUrl } = props
-  const location = useLocation()
-  const lastHashRef = useRef<string>("")
+	const { tournament, logoUrl } = props
+	const location = useLocation()
+	const lastHashRef = useRef<string>("")
 
-  useEffect(() => {
-    if (location.hash) {
-      lastHashRef.current = location.hash.slice(1)
-    }
+	useEffect(() => {
+		if (location.hash) {
+			lastHashRef.current = location.hash.slice(1)
+		}
 
-    if (lastHashRef.current && document.getElementById(lastHashRef.current)) {
-      document.getElementById(lastHashRef.current)?.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
-      })
-    }
-  }, [location])
+		if (lastHashRef.current && document.getElementById(lastHashRef.current)) {
+			document.getElementById(lastHashRef.current)?.scrollIntoView({
+				behavior: "smooth",
+				block: "center",
+				inline: "center",
+			})
+		}
+	}, [location])
 
-  return (
-    <div id={tournament.systemName} style={{ position: "relative" }}>
-      <h4 className="text-secondary mb-3">
-        {constants.EventCalendarYear} {tournament.name}
-      </h4>
-      {logoUrl && (
-        <LogoImage>
-          <img src={logoUrl} alt={`${tournament.name} host`} />
-        </LogoImage>
-      )}
-      <MarkdownRender text={tournament.description} />
-      <NavLink
-        to={`/tournaments/detail/${tournament.systemName}/${constants.EventCalendarYear}`}
-        className="nav-link"
-      >
-        {constants.EventCalendarYear} Tournament Details
-      </NavLink>
-      <NavLink to={`/tournaments/history/${tournament.systemName}`} className="nav-link">
-        Past {tournament.name} Winners
-      </NavLink>
-    </div>
-  )
+	return (
+		<div id={tournament.systemName} style={{ position: "relative" }}>
+			<h4 className="text-secondary mb-3">
+				{constants.EventCalendarYear} {tournament.name}
+			</h4>
+			{logoUrl && (
+				<LogoImage>
+					<img src={logoUrl} alt={`${tournament.name} host`} />
+				</LogoImage>
+			)}
+			<MarkdownRender text={tournament.description} />
+			<NavLink
+				to={`/tournaments/detail/${tournament.systemName}/${constants.EventCalendarYear}`}
+				className="nav-link text-info"
+			>
+				{constants.EventCalendarYear} Tournament Details
+			</NavLink>
+			<NavLink
+				to={`/tournaments/history/${tournament.systemName}`}
+				className="nav-link text-info mt-2"
+			>
+				Past {tournament.name} Winners
+			</NavLink>
+		</div>
+	)
 }
 
 export default TournamentView
